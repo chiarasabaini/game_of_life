a6d464a2f6d567da9164397702916a53c5d145a5
chiarasabaini <chiara@sabaini.com>
2020-05-28 14:23:19 +0200

    Initial commit

---
+++ 02.01/cli.py
@@ -0,0 +1,90 @@
+__author__ = "chiara@sabaini.com"
+__version__ = "2.0"
+__date__ = "2020-05-07"
+
+import time
+import random
+from copy import deepcopy
+board = []
+ALIVE = 1
+DEAD = 0
+
+def board_init(h, w):
+    """Initialize the board, assigning a random state (1: alive, 0: dead) to every cell."""
+
+    global board
+    board = []
+
+    for y in range(h):
+        row = []
+
+        for x in range(w):
+            row.append(random.randint(0, 1))
+
+        board.append(row)
+
+def count_neighbours(x, y):
+    """Counts how many cells around the one with the given coordinates are alive."""
+
+    neighbours = 0
+
+    for i in range(x - 1, x + 2):
+        if i > 0:
+            for j in range(y - 1, y + 2):
+                if j > 0:
+                    if not (j == y and i == x):
+                        try:
+                            if board[i][j] == ALIVE:
+                                neighbours += 1
+                        except:
+                            pass
+
+    return neighbours
+
+def update():
+    """Updates the board"""
+
+    global board
+    next_board = deepcopy(board)
+    
+    for x in range(len(board)):
+        for y in range(len(board[x])):
+            neighbours = count_neighbours(x ,y)
+
+            if board[x][y] == ALIVE and (neighbours < 2 or neighbours > 3):
+                next_board[x][y] = 0
+
+            if board[x][y] == DEAD and neighbours == 3:
+                next_board[x][y] = 1
+
+    board = next_board
+
+def show(board):
+    """Shows the board with * where the cell is alive and a space when the cell is dead."""
+
+    for x in range(len(board)):
+        for y in range(len(board[x])):
+            if board[x][y] == 1:
+                print("*", end="")
+            else:
+                print(" ", end="")
+
+        print()
+
+    print("---")
+    
+def game_of_life(h, w):
+    """This function is the one that make the game run"""
+    board_init(h, w)
+    show(board)
+
+    while True:
+        update()
+        show(board)
+        time.sleep(0.25)
+    
+def main(h, w):
+    game_of_life(h, w)
+
+if __name__ == "__main__":
+    main(8, 12)
---
+++ 02.01/gui.py
@@ -0,0 +1,59 @@
+__author__ = "chiara@sabaini.com"
+__version__ = "2.0"
+__date__ = "2020-05-07"
+
+import pygame
+import cli as gol
+import pygame.locals as pl
+
+pygame.init()
+
+GAME_RES = WIDTH, HEIGHT = 256, 256
+FPS = 30
+CELL_SIZE = 4
+CELL_COLOR = (55, 0, 255)
+GAME_TITLE = 'GAME OF LIFE by Chiara Sabaini'
+
+window = pygame.display.set_mode(GAME_RES, pl.HWACCEL|pl.HWSURFACE|pl.DOUBLEBUF) # Sets the window
+pygame.display.set_caption(GAME_TITLE) # Sets the game title
+clock = pygame.time.Clock()
+
+# Game Values
+
+background_color = (0, 0, 0) # RGB value
+gol.board_init(WIDTH, HEIGHT)
+
+# End of Game Values
+
+# Game loop
+game_running = True
+while game_running:
+
+    # Event handling
+    for event in pygame.event.get():
+        if event.type == pl.QUIT:
+            game_running  = False
+            break
+        if event.type == pl.KEYDOWN:
+            if event.key == pl.K_ESCAPE:
+                game_running  = False
+                break
+            if event.key == pl.K_SPACE:
+                gol.board_init(HEIGHT // CELL_SIZE, WIDTH // CELL_SIZE) # Reinitializes the board when the space key is pressed
+
+    # Game logic
+    gol.update()
+
+    # Display update
+    pygame.Surface.fill(window, background_color)
+    
+    for x in range(len(gol.board)):
+        for y in range(len(gol.board[x])):
+            if gol.board[x][y] == 1:
+                pygame.draw.rect(window, CELL_COLOR, [x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE])
+
+    pygame.display.update()
+    clock.tick(FPS)
+
+pygame.quit()
+exit(0)
